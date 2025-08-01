203faa3f30b983246b041d6ff2ef9fb04c863d6f
MasoumehTafvizi <masoumeh.tafviziii@gmail.com>
2025-08-01 16:18:00 +0200

    Control Structures

--- files/test.py
+++
@@ -1,8 +0,0 @@
-def greet(name):
-    greeting = f"Hello, {name}!"
-    return greeting
-
-def main():
-    user_name = input("Enter your name: ")
-    message = greet(user_name)
-    print(message)
