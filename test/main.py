e2e6eb205bc3de92946b20f46085fb82642fc2c9
MasoumehTafvizi <masoumeh.tafviziii@gmail.com>
2025-09-10 14:54:06 +0200

    Modular Programming: Creating and using packages

--- test/main.py
+++ test/main.py
@@ -1,21 +1,10 @@
 import os
-import random
 import sys
 
-import isort
 import numpy as np
-import parso
-import my_math 
-from new_one import my_function
 
-os
-sys
-my_math
-my_function
-np
-parso
-isort
-random
-isort
-random
+from utils.math.my_math import add
 
+#sys.modules['numpy']
+print(sys.path)
+print(sys.modules['numpy'])
