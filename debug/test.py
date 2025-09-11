7ff8c099b0879ce2e88d312d357dd1b97144bb23
MasoumehTafvizi <masoumeh.tafviziii@gmail.com>
2025-09-11 14:43:21 +0200

    Modular Programming: Debigging techniques with VScode

---
+++ debug/test.py
@@ -0,0 +1,10 @@
+def greet(name):
+    greeting = f"Hello, {name}!"
+    return greeting
+
+
+    
+if __name__ == "__main__":
+    names = ["Alice", "Bob", "Charlie", "Diana brown", "Ethan white", "Fiona", "George king"]
+    for name in names:
+        greet(name) 
